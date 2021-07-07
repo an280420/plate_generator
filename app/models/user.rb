1223893d15c40a962cf9cde47d2e10ffe64d561a
@an280420 <angit280420@gmail.com>
2021-07-07 09:31:33 +0500

    changing email to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  FORMAT_USERNAME = /\A\w+\z/
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, length: { maximum: 40 }, format: { with: FORMAT_USERNAME }
+  validates :name, uniqueness: true
   has_many :courses
 end
