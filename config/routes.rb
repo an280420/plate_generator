7a15b2ac7dd5b50ae7c64cb39cc36810f3f82a48
@an280420 <angit280420@gmail.com>
2021-07-14 23:54:06 +0500

    edit route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :templates
   resources :courses
+  get '/courses/:id/pdf', to: 'courses#print_pdf'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
