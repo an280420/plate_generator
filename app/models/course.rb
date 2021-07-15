78c9c2bccb78e4bf67670683e99b7cac4ed0a16f
@an280420 <angit280420@gmail.com>
2021-07-15 11:25:24 +0500

    Add validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :template
-  belongs_to :user 
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,7 @@
 class Template < ApplicationRecord
   has_many :courses, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :body, presence: true
 end
