7baaef2f8b2ea05e14cd7d77b80276cd413b0240
@an280420 <angit280420@gmail.com>
2021-09-28 01:15:28 +0500

    Add tests for models

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:course) { FactoryBot.create(:course) }
+
+  it 'return name course' do
+    expect(course.name.split("№")[0]).to eq('Курс ')
+    expect(course.name.split("№")[1].to_i).to be_between(0, 999)
+  end
+
+  it 'return level course' do
+    expect(%w(soft medium hard)).to include(course.level) 
+  end
+
+  it 'return volume course' do
+    expect(course.volume.split(" ")[0].to_i).to be_between(0, 999)
+    expect(course.volume.split(" ")[1]).to eq('hours')
+  end
 end
